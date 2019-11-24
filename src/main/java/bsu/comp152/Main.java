3ce9feeb4fac0c2de05214e387236f027332d49c
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-11-24 16:29:21 -0500

    Added to the Main.java class. Just a bit

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,7 +1,11 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -11,6 +15,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        Parent root = null;
+        var loc = getClass().getResource("Main.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e) {
+            System.out.println("Need the FXML file");
+        }
+        
     }
 }

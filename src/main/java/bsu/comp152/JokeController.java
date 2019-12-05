00693559b34410125f9b1aa6db1908901a419851
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-12-04 20:36:57 -0500

    Some work I've done in my classes. I have made progress mainly on the JokeDataHandler, with some adjustments to the controller and fxml.

--- src/main/java/bsu/comp152/JokeController.java
+++ src/main/java/bsu/comp152/JokeController.java
@@ -1,4 +1,14 @@
 package bsu.comp152;
 
-public class JokeController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class JokeController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    //fill here
 }
--- src/main/java/bsu/comp152/JokeDataHandler.java
+++ src/main/java/bsu/comp152/JokeDataHandler.java
@@ -1,4 +1,54 @@
 package bsu.comp152;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 public class JokeDataHandler {
+    private HttpClient dataGrabber;
+    private String webLocation;
+
+    public JokeDataHandler(String webLocation){
+        dataGrabber = HttpClient.newHttpClient();
+        this.webLocation = webLocation;
+    }
+
+    public ArrayList<jokeDataType> getData{
+        var requestBuilder = HttpRequest.newBuilder();
+        var dataRequest = requestBuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response = null;
+        try{
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }
+        catch(IOException e){
+            System.out.println("Cannot connect to site");
+        }
+        catch(InterruptedException e){
+            System.out.println("Eh, broke connection");
+        }
+        if (response == null){
+            System.out.println("UHHH");
+            System.exit(-1);
+        }
+        var goodData = response.body();
+        //var jsonInterpreter = new Gson();
+    }
+
+    class responseDataType{
+        String title;
+        float version;
+        String href;
+        ArrayList<jokeDataType> results;
+    }
+
+    class jokeDataType{
+        String category;
+        String type;
+        String joke;
+        String setup;
+        String delivery;
+    }
 }
